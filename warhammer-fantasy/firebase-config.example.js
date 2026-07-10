5093f522d56d5dae65a0a915c4d6dc9d715c9df5
Lenny-glitch <jonathanlollar@gmail.com>
2026-07-10 13:37:14 -0700

    fantasy 40k and killteam merge 7/10/26

--- warhammer-fantasy/firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
